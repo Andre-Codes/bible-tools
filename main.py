b5f43d8c0e5203d4f206f55a34e45ac7fa3a1d16
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-07-21 14:21:29 -0500

    fix: added comments

--- main.py
+++ main.py
@@ -9,22 +9,23 @@ bible_df = pd.read_csv(main_csv_path)
 crossref_df = pd.read_csv(cf_csv_path)
 
 ####################################
-# EXAMPLE return
+# EXAMPLE - convert a verse ID into verse reference
 # Enter a verse id in format book-chapter-verse (John 3:16 = 43 003 016)
-# convert a verse ID into verse reference
-reference = tools.id_to_reference(bible_df, 43003016)
-print(reference)
+
+reference = tools.id_to_reference(bible_df, 43003016)  # Result: ('John', 3, 16, 'John 3:16')
+
+# Convert standard verse reference to its ID
+reference_id = tools.reference_to_id(crossref_df, "John", 3, 16)  # result: 43003016
 
 ####################################
-# EXAMPLE find cross references
-reference_id = tools.reference_to_id(crossref_df, "John", 3, 16)
-cross_refs = tools.find_cross_references(crossref_df, reference_id)
+# FEAT EXAMPLE - find cross-references
+cross_refs = tools.find_cross_references(crossref_df, 43003016)
 result = cross_refs[['Book_Name', 'Chapter', 'Verse', 'origin_text']]
 result.head()
 
 ####################################
-# EXAMPLE find matching verses given word list
-verse_search_df = bible_df.copy()  # Save copy of unaltered df
+# FEAT EXAMPLE - find matching verses given word list
+verse_search_df = bible_df.copy()  # Copy the unaltered df
 
 # Remove all punctuation
 verse_search_df['origin_text'] = verse_search_df['origin_text'].str.replace('[^\w\s]', '', regex=True)
@@ -36,6 +37,7 @@ verse_search_df['origin_text'] = verse_search_df['origin_text'].str.split()
 lower_if_string = lambda verse: [word.lower() if isinstance(word, str) else word for word in verse]
 verse_search_df['origin_text'] = verse_search_df['origin_text'].apply(lower_if_string)
 
+# Find verses that contain at least 50% of the words in the list
 word_list = ["everlasting", "Israel", "covenant", "God", "seed", "children"]
 word_list = list(map(str.lower, word_list))
 results = tools.verse_search(verse_search_df, word_list, 50)
